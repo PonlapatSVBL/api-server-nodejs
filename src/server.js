3854e368b4aba491049546a922ab2d0a58d2996e
human.soft <human.soft@hotmail.com>
2023-06-17 16:50:02 +0700

    rm cors, try-catch

--- src/server.js
+++ src/server.js
@@ -6,37 +6,30 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { connection } = require('./config/connector');
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.raw({ type: 'application/json' }));
 
 app.get('/rabbit-dev/api', (req, res) => {
-    if (req.body) {
+    try {
         let body = JSON.parse(req.body);
-        // console.log(body);
-
         const apiPath = path.join(__dirname, 'modules', body.compgrp, body.comp, `${body.action}.js`);
-        // console.log(apiPath);
 
-        try {
-            const api = require(apiPath);
-            api(req, res);
-        } catch (error) {
-            res.status(404).json({ error: 'API not found' });
-        }
+        const api = require(apiPath);
+        api(req, res);
+    } catch (error) {
+        res.status(404).json({ error: 'API not found' });
     }
 });
 
 app.post('/rabbit-dev/api', (req, res) => {
-    if (req.body) {
+    try {
         let body = JSON.parse(req.body);
         const apiPath = path.join(__dirname, 'modules', body.compgrp, body.comp, `${body.action}.js`);
 
-        try {
-            const api = require(apiPath);
-            api(req, res);
-        } catch (error) {
-            res.status(404).json({ error: 'API not found' });
-        }
+        const api = require(apiPath);
+        api(req, res);
+    } catch (error) {
+        res.status(404).json({ error: 'API not found' });
     }
 });
 
